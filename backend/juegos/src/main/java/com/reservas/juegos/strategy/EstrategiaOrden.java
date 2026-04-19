7d2ef5494a3e011ada04605c596897725316f8cc
KVH14 <karlavargashernandez1596@gmail.com>
2026-04-18 21:59:04 -0500

    Cambio en los archivos

--- backend/juegos/src/main/java/com/reservas/juegos/strategy/EstrategiaOrden.java
+++
@@ -1,8 +0,0 @@
-package com.reservas.juegos.strategy;
-
-import java.util.List;
-import com.reservas.juegos.entities.Tarea;
-
-public interface EstrategiaOrden {
-    void ordenar(List<Tarea> tareas);
-}
