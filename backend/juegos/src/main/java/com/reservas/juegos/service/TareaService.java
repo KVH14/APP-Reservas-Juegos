7a3346b973e957410856829d95adea6de557849d
JuanD-777 <juandicastrillon2206@gmail.com>
2026-04-18 22:34:54 -0500

    Introduccion de la interfaz observer

--- backend/juegos/src/main/java/com/reservas/juegos/service/TareaService.java
+++ backend/juegos/src/main/java/com/reservas/juegos/service/TareaService.java
@@ -1,6 +1,8 @@
 package com.reservas.juegos.service;
 
 import com.reservas.juegos.entities.Tarea;
+import com.reservas.juegos.observer.TareaObserver;
+import com.reservas.juegos.strategy.EstrategiaOrden;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,6 +11,7 @@ import java.util.List;
 @Service
 public class TareaService {
     private final List<Tarea> tareas = new ArrayList<>();
+
 
     public List<Tarea> listar() {
         return tareas;
